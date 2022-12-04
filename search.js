62507eeba9ae283e50f245d95c3f537f358baf98
jkim019 <113134373+jkim019@users.noreply.github.com>
2022-12-04 02:31:55 -0500

    bruh whater

--- search.js
+++ search.js
@@ -11,19 +11,41 @@ http.createServer(function (req, res) {
   if (req.url == "/search")
   {
     // listen to the html, and get the select type
+    res.writeHead(200, { "Content-Type": "text/html" });
+    read_html = "";
+    req
+      .on("data", (data) => {
+        read_html += data.toString();
+      })
+      .on("end", () => {
+        read_html = qs.parse(read_html);
+        var searchtype = read_html["name_or_ticker"];
+        var key = read_html["user_input"];
 
+        var result = "<h1>RESULTS</h1>";
+        MongoClient.connect(url, { useUnifiedTopology: true }, (err, database) => {
+          if(err) { console.log("Connection err: " + err); return; }
+
+          // Otherwise, Connected, keep going
+          var dbo = database.db("StockTicker");
+          var collection = dbo.collection("equities");
+
+          // check type
+          if (searchtype == "name")
+          {
+            // get ticker of inputted company
+            var found = await collection.find({company: key}).toArray();
+          } else { // search by ticker
+            var found = await collection.find({ticker: key}).toArray();
+          }
+
+          for (var i = 0; i<found.length(); i++)
+          {
+            result += "found[i].name" + "found[i].ticker" ;
+          }
+
+        res.end(result);
+        database.close();
+      });
   }
-
-}).listen(8080); // local server
-
-// initiate search on my mongo
-async function searchMongo(){
-  var result = "<h1>RESULTS</h1>";
-  MongoClient.connect(url, { useUnifiedTopology: true }, (err, database) => {
-    if(err) { console.log("Connection err: " + err); return; }
-
-    // Otherwise, Connected, keep going
-
-
-  }
-});
+}); // local server
